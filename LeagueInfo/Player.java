b04e1265f97ea331802523eeaa90267b78b52c0a
Scott Lesczynski <lesczynskiscott311@gmail.com>
2023-04-23 22:23:01 -0500

    implement first iteration of player and goalie

--- LeagueInfo/Player.java
+++ LeagueInfo/Player.java
@@ -1,5 +1,25 @@
 package LeagueInfo;
-
 public class Player {
-    
+
+    public String name;
+    public String position;
+    public int skatingSkill;
+    public int shootingSkill;
+    public int defenseSkill;
+    public int goalieSkill;
+    public int height = 100;
+    public int weight = 100;
+
+    Player(String newName, String newPosition){
+
+        this.name = newName;
+        this.position = newPosition;
+        if(newPosition == "Skater"){
+            this.skatingSkill = (int) (Math.random() * (99 - 40)) + 40;
+            this.shootingSkill = (int) (Math.random() * (99 - 40)) + 40;
+            this.defenseSkill = (int) (Math.random() * (99 - 40)) + 40;
+        } else {
+            this.goalieSkill = (int) (Math.random() * (99 - 40)) + 40;
+        }
+    }
 }
--- LeagueInfo/Team.java
+++ LeagueInfo/Team.java
@@ -3,11 +3,18 @@ package LeagueInfo;
 public class Team {
     public String city;
     public String name;
-    public Player[] roster;
-    public int offense;
-    public int defense;
-    public int goalie;
+
+    public Player[] roster = new Player[50];
+
+    public int numSkaters = 0;
+    public int offense = 0;
+    public int defense = 0;
+
+    public int numGoalies = 0;
+    public int goalie = 0
+    ;
     public int overall;
+
     public int wins = 0;
     public int loses = 0;  
 
@@ -15,9 +22,31 @@ public class Team {
     
             //Random Number with bounds: int randomNumber = (int) (Math.random() * (upper - lower)) + lower;
             this.name = inputName;
-            this.offense = (int) (Math.random() * (100 - 50) + 50);
-            this.defense = (int) (Math.random() * (100 - 50) + 50);
-            this.goalie = (int) (Math.random() * (100 - 50) + 50);
+
+            for(int i = 0; i < 47; i++){
+                String tempName = this.name + i;
+                roster[i] = new Player(tempName, "Skater");
+            }
+
+            for(int i = 47; i < 50; i++){
+                String tempName = this.name + i;
+                roster[i] = new Player(tempName, "Goalie");
+            }
+
+            for(int i = 0; i < 50; i++){
+                if(roster[i].position.toString() == "Skater"){
+                    this.offense+=(roster[i].skatingSkill + roster[i].shootingSkill)/2;
+                    this.defense+=(roster[i].defenseSkill);
+                    numSkaters++;
+                } else {
+                    this.goalie+=(roster[i].goalieSkill);
+                    numGoalies++;
+                }
+            }
+
+            this.offense = this.offense/numSkaters;
+            this.defense = this.defense/numSkaters;
+            this.goalie = this.goalie/numGoalies;
             this.overall = this.offense + this.defense + this.goalie;
     }
 }
